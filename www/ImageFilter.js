8781dbea28acb2cf1e51951cbea06ee12a78504a
Loaay Elkherbawy <loaay.alkherbawy@gmail.com>
2020-07-02 02:51:19 +0200

    Image Filter update

--- www/ImageFilter.js
+++ www/ImageFilter.js
@@ -1,17 +1,15 @@
 var exec = require('cordova/exec');
 
-exports.applyEffect = function(options, success, error) {
-    if(options.filters && options.filters.length > 0){
-      applyfil(options,"applyEffect",options.filters[0],options.path,0,success,error)
-    }
-    else exec(success, error, "ImageFilter", "applyEffect", [options.path, options.filter,options.weight, options.quality, 1]);
+exports.applyEffect = function(options) {
+  return new Promise(function (resolve, reject) {
+    exec(resolve, reject, "ImageFilter", "applyEffect", [options.path, options.filters, options.quality, 1]);
+  });
 };
 
-exports.applyEffectForReview = function(options, success, error) {
-  if(options.filters && options.filters.length > 0){
-    applyfil(options,"applyEffectForReview",options.filters[0],options.path,0,success,error)
-  }
-  else exec(success, error, "ImageFilter", "applyEffectForReview", [options.path, options.filter,options.weight, options.quality, 1]);
+exports.applyEffectForReview = function(options) {
+  return new Promise(function (resolve, reject) {
+    exec(resolve, reject, "ImageFilter", "applyEffectForReview", [options.path, options.filters, options.quality, 1]);
+  });
 };
 
 exports.cropImage = function(success, fail, image, options) {
@@ -32,20 +30,8 @@ exports.cropAsync = function(image, options) {
   });
 }
 
-exports.applyEffectForThumbnail = function(options, success, error) {
-  if(options.filters && options.filters.length > 0){
-    applyfil(options,"applyEffectForThumbnail",options.filters[0],options.path,0,success,error)
-  }
-  else exec(success, error, "ImageFilter", "applyEffectForThumbnail", [options.path, options.filter,options.weight, options.quality, 1]);
+exports.applyEffectForThumbnail = function(options) {
+  return new Promise(function (resolve, reject) {
+    exec(resolve, reject, "ImageFilter", "applyEffectForThumbnail", [options.path, options.filters, options.quality, 1]);
+  });
 };
-
-function applyfil(options,action,item,path,index,success,error){
-  if(index === options.filters.length - 1){
-    exec(success,error, "ImageFilter", action, [path, item.filter,item.weight, options.quality, 1]);
-  }else{
-    exec(function(img){
-      var nextItem = index+1;
-      applyfil(options,action,options.filters[nextItem],img,nextItem,success,error);
-    }, function(err){}, "ImageFilter", action, [path, item.filter,item.weight, options.quality, 1]);
-  }
-}
